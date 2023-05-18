8ceab7f2f9760836760a8a8036cd15e4bef7b317
Hniii <h.tatoult@esi-sba.dz>
2023-05-18 14:11:21 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:music/provider/music_model_provider.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+
+import 'Screens/AllSongs.dart';
+import 'Screens/NowPlaying.dart';
+
+Future<void> main() async {
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => SongModelProvider(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Player',
+      debugShowCheckedModeBanner: false,
+     theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.purple,
+      primarySwatch: Colors.purple,
+    ),
+      home: const AllSongs(),
+    );
+  }
+}
